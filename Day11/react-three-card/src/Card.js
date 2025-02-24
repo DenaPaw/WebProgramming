a86bb0dc4c52258fb9691fce2deee06788f8be51
Dena Paw <dpaw@atu.edu>
2025-02-24 13:54:57 -0600

    Work on react and three card problem

---
+++ Day11/react-three-card/src/Card.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Card.css"; //Import CSS for styling
+import { title } from "process";
+
+function Card({title, content, color}){
+    return (
+        <div className="card" style = {{backgroundColor: color}}>
+            <h2>{title}</h2>
+            <p>{content}</p>
+            </div>
+    )
+}
--- README.md
+++ README.md
@@ -12,3 +12,11 @@ Day 5: javascripe
 Day 6:
 
 Day 7:
+
+Day 8:Javascript Forms
+
+Day 9:JSON
+
+Day 10:Fetch API Example
+
+Day 11:wordked on React and worked on the three card problem
