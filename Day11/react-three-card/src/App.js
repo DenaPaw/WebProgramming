35760200ecb45cb82deae521b22663e727333d9d
Dena Paw <dpaw@atu.edu>
2025-02-26 21:57:23 -0600

    update app.j to reflect funtio call. also updated the CSS for Card and App

--- Day11/react-three-card/src/App.js
+++ Day11/react-three-card/src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
-import Card from"./Card";
+import Card from "./Card";
 import "./App.css";
 
-function App(){
+function App() {
   const cards = [
-    {title: "Card 1", content : "First", color : "white"},
-    {title: "Card 2", content : "Second", color : "white"},
-    {title: "Card 3", content : "Third", color : "white"},
+    { title: "Card 1", content: "First", color: "black" },
+    { title: "Card 2", content: "Second", color: "green" },
+    { title: "Card 3", content: "Third", color: "blue" },
   ];
 
-  return(
+  return (
     <div className="container">
-      {
-        cards.map((card,index) => (
-          <Card key={index}{...card}/>
-      ))
-      }
+      {cards.map((card, index) => (
+        <Card key={index} {...card} />
+      ))}
     </div>
-  )
+  );
 }
+
+export default App;
